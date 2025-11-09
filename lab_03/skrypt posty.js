90379da189ddd4245b4ffa4459a0364a8f34b344
Michał Mąka <mmaka@student.agh.edu.pl>
2025-11-09 20:10:18 +0100

    finish dummyjson excercise

--- lab_03/skrypt posty.js
+++ lab_03/skrypt posty.js
@@ -5,39 +5,88 @@ fetch("https://dummyjson.com/posts")
   .then((data) => {
     const T = data.posts;
     const container = document.createElement("div");
+    const COLOR_LIMIT = 40;
 
     T.forEach((element) => {
       const post = document.createElement("div"); //nowy post
-      post.classList.add("post"); //dodaj do post cos
-      post.innerHTML = element.id + " " + element.title + " " + element.body; // jak ma wygladac w html
-      container.appendChild(post);
+      post.classList.add("post"); //nadaj postowi klases post
+      post.innerHTML =
+        element.id + '. "' + element.title + '"<br />' + " " + element.body; // jak ma wygladac w html
+      container.appendChild(post); //dodaj do container jako child
 
-      // post.addEventListener("click", () => {
-      //   mainPost.innerHTML = element.post;
-      // });
+      const ratings = document.createElement("div"); // opakowanie na opinie
+      ratings.classList.add("ratings"); //nadaj odpowiednia klase
+      post.appendChild(ratings); //dodaj do post
 
-      const button = document.createElement("button"); //nowy button
-      button.textContent = "like";
-      const like_count = document.createElement("div"); // licznik like
+      const likes = document.createElement("div");
+      likes.classList.add("likes");
+      likes.innerHTML = "amount of likes 👍: ";
+      ratings.appendChild(likes);
 
-      const button2 = document.createElement("button"); //nowy button
-      button2.textContent = "dislike";
-      const dislike_count = document.createElement("div"); // licznik dislike
+      const button_like_add = document.createElement("button");
+      button_like_add.textContent = "+"; // nadaj tekst na button
+      const button_like_remove = document.createElement("button");
+      button_like_remove.textContent = "-";
 
-      console.log(post);
-      document.body.appendChild(post);
-      document.body.appendChild(button);
-      document.body.appendChild(button2);
+      const like_count = document.createElement("span");
+      like_count.textContent = element.reactions.likes; // pobierz like z elementu z jsona
 
-      button.addEventListener("click", () => {});
-      button2.addEventListener("click", () => {
-        // mainPost.innerHTML = element.post;
-        dislike_count;
+      likes.appendChild(button_like_add);
+      likes.appendChild(like_count);
+      likes.appendChild(button_like_remove);
+
+      // --------------------
+
+      const dislikes = document.createElement("div");
+      dislikes.classList.add("dislikes");
+      dislikes.innerHTML = "amount of dislikes 👎: ";
+      ratings.appendChild(dislikes);
+
+      const button_dislike_add = document.createElement("button");
+      button_dislike_add.textContent = "+";
+      const button_dislike_remove = document.createElement("button");
+      button_dislike_remove.textContent = "-";
+
+      const dislike_count = document.createElement("span");
+      dislike_count.textContent = element.reactions.dislikes;
+
+      if (parseInt(dislike_count.textContent) >= COLOR_LIMIT) {
+        post.classList.add("post-bad");
+      }
+
+      dislikes.appendChild(button_dislike_add);
+      dislikes.appendChild(dislike_count);
+      dislikes.appendChild(button_dislike_remove);
+
+      // logika klikania
+      button_like_add.addEventListener("click", () => {
+        let currentCount = parseInt(like_count.textContent);
+        like_count.textContent = currentCount + 1;
       });
-    });
-    // document.body.appendChild(container);
-  });
+      button_like_remove.addEventListener("click", () => {
+        let currentCount = parseInt(like_count.textContent);
+        if (currentCount > 0) {
+          like_count.textContent = currentCount - 1;
+        }
+      });
 
-function add1() {
-  document.getElementById("");
-}
+      button_dislike_add.addEventListener("click", () => {
+        let currentCount = parseInt(dislike_count.textContent);
+        dislike_count.textContent = currentCount + 1;
+        if (currentCount + 1 >= COLOR_LIMIT) {
+          post.classList.add("post-bad");
+        }
+      });
+      button_dislike_remove.addEventListener("click", () => {
+        let currentCount = parseInt(dislike_count.textContent);
+        if (currentCount > 0) {
+          dislike_count.textContent = currentCount - 1;
+          if (currentCount - 1 < COLOR_LIMIT) {
+            post.classList.remove("post-bad");
+          }
+        }
+      });
+      console.log(post);
+    });
+    document.body.appendChild(container);
+  });
